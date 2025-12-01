5fe333d681690105b47cf879b64bad40eb5eacda
Sam Tyurenkov <sam.tyurenkov@gmail.com>
2025-12-01 14:38:05 +0300

    jupyter

--- docker/runpod-desktop/entry.sh
+++ docker/runpod-desktop/entry.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Start the XRDP service
-/usr/sbin/xrdp -n -f &
-
-# Optionally, add additional setup steps here
-echo "XRDP service started."
-
 # Create a non-root user for remote desktop access
 adduser --disabled-password --gecos "" amt && \
 echo "amt:$DESKTOP_PASS" | chpasswd && \
 adduser amt sudo
+
+start_xrdp() {
+    echo "XRDP service started."
+    /usr/sbin/xrdp -n -f &
+}
+
+start_jupyterlab() {
+    echo "Starting Jupyter Lab..."
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=*
+}
+
+start_xrdp &
+start_jupyterlab
 
 # Keep the container running indefinitely
 tail -f /dev/null
