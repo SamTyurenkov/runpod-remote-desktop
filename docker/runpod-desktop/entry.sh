4fe8eef6ed9a4ed1b433be4c1e2cac23fb726b55
Sam Tyurenkov <sam.tyurenkov@gmail.com>
2025-12-29 18:44:35 +0300

    fixes

--- docker/runpod-desktop/entry.sh
+++ docker/runpod-desktop/entry.sh
@@ -10,6 +10,6 @@ mkdir -p /home/amt/Desktop
 chown amt:amt /home/amt/Desktop/*.desktop
 chmod +x /home/amt/Desktop/*.desktop
 
-xpra start :100 --start=google-chrome --bind-tcp=0.0.0.0:14500 --html=on &
+xpra start :100 --start="google-chrome --no-sandbox --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --disable-features=VizDisplayCompositor" --bind-tcp=0.0.0.0:14500 --html=on &
 xpra start :101 --start=telegram-desktop --bind-tcp=0.0.0.0:14501 --html=on &
 tail -f /dev/null
